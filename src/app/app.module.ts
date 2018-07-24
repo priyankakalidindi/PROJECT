910ffd40768a32a32aed2313cf6f1759a704baa1
Priyanka Koteshwari Kalidindi <priyankakalidindi23@gmail.com>
2018-07-24 10:01:10 +0530

    changes made in src

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatAutocompleteModule,MatFormFieldModule,MatFormFieldControl,
+  MatInputModule, MatSnackBarModule, MatMenuModule, MatButtonModule,
+  MatIconModule, MatSlideToggleModule, MatSliderModule} from '@angular/material';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+import { SampleComponent } from './sample/sample.component';
+import { NestedComponent } from './nested/nested.component';
+import { ToggleComponent } from './toggle/toggle.component';
+import { SliderComponent } from './slider/slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SampleComponent,
+    NestedComponent,
+    ToggleComponent,
+    SliderComponent
+   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatSnackBarModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSlideToggleModule,
+    MatSliderModule
+    
+
   ],
+  exports: [MatSnackBarModule],
+  
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+ 
 })
-export class AppModule { }
+  
+  
+export class AppModule { 
+  
+}
